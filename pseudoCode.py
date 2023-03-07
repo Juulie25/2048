0dbc4bc93a0090ea3ef1c7140ab0e173d7118ee9
julie pessey <pessey3@orange.fr>
2023-03-07 11:41:04 +0100

    début implémentation evaluate

--- pseudoCode.py
+++ pseudoCode.py
@@ -1,18 +1,127 @@
 #Défintion des variables 
 # s -> configuration actuelle du plateau (temps t)
+# s == grid
+
 # s0 -> configuration du plateau a t+1, lorsque l'action de déplacement a été faite = AFTER STATE
-# s00 -> configuration du plteau a t+2, une nouvelle tuile est ajoutée 
+# s00 -> configuration du plateau a t+2, une nouvelle tuile est ajoutée
 # r -> reward : 
 # a -> action réalisée (G/D/H/B)
 # P(x) -> fonction de transition 
 # R(x) -> fonction de reward 
+import numpy
+import pygame
 
+def __init__(self):
+    # grille complète
+    self.grid = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]
+
+    self.tilesColor = {
+        0: "#BFB3A5",
+        2: "#FAE7E0",
+        4: "#FAE5CA",
+        8: "#FBB17A",
+        16: "#DF9F74",
+        32: "#E08A72",
+        64: "#FD5B42",
+        128: "#FAD177",
+        256: "#F7D067",
+        512: "#F9CA58",
+        1024: "#F9CA58",
+        2048: "#FBC52D",
+        4096: "#F66574",
+        8192: "#F34B5C",
+        16384: "#EB4039",
+        32768: "#70B3D8",
+        65536: "#5EA1E4",
+        131072: "#007FC2"
+    }
+
+    self.actions = {
+        "UP",
+        "DOWN",
+        "LEFT",
+        "RIGHT"
+    }
+
+    self.score = 0
+    self.nbMove = 0
+    self.add_new_tile()
+    self.add_new_tile()
+
+    pygame.init()
+    self.font = pygame.font.SysFont("Arial", 36)
+    self.screen = pygame.display.set_mode((400, 500))
+    pygame.display.set_caption("2048 Game")
+
+    while not is_game_over():
+        action = evaluate()
+        self.score += action_choisie
+
+
+def is_game_over(self):
+    empty_cells = [(i, j) for i in range(4) for j in range(4) if self.grid[i][j] == 0]
+    if empty_cells: return False
+    for i in range(3):
+        for j in range(3):
+            if self.grid[i][j] == self.grid[i + 1][j] or self.grid[i][j] == self.grid[i][j + 1]:
+                return False
+    # Check the last row and column separately
+    for i in range(3):
+        if self.grid[3][i] == self.grid[3][i + 1]:
+            return False
+        if self.grid[i][3] == self.grid[i + 1][3]:
+            return False
+    return True
+
+# TODO : implémneter la fonction entièrement
+#test tous les actions possibles et renvoie la meilleure
+# 1. on regarde quelle action renvoie le meilleure score
+# 2. si egalité alors on regarde la place libérée
+# 3. si éaglité alors on regarde celle qui crée le moins de trou
+def evaluate(self):
+    action_choisie = None
+    score = 0
+    espace_libre = 0
+    trous = 0
+
+    #TODO: créer un nouvel objet qui reprend les valeurs de la grille actuelle
+    grid_copy = numpy.array([[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
+
+    #TODO :revoir l'appel des mouvements
+    for action in self.actions:
+        score_tmp = move_tiles_left()
+        if score_tmp > score :
+            score = score_tmp
+            action_choisie = action
+        else :
+            if score == score_tmp:
+                # regarder la place libérée
+                # si égalité alors
+                    # nombre de trous créés
+    return action_choisie
+
+def move_tiles_left(self):
+    score = 0
+    for row in self.grid:
+        row.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
+        for i in range(3):
+            if row[i] == row[i + 1]:
+                row[i] *= 2
+                score += row[i]
+                row[i + 1] = 0
+        row.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
+    return score
 
 def playGame() :
     score = 0
     s = INITIALIZE GAME STATE
     while (not IS TERMINAL STATE(s)):
-        a =  arg maxa0∈A(s) EVALUATE(s, a0)
+        a =  arg maxa0∈A(s) EVALUATE(s, a)
         r, s0, s00 = MAKE MOVE(s, a)
     if LEARNING ENABLED :
         LEARN EVALUATION(s, a, r, s0, s00)
@@ -31,6 +140,7 @@ def makeMove(s,a):
 #EVALUATE 
 # attempts to measure the utility of taking each possible action a ∈ A(s) in the current state s
 # moves are selected to maximize the value returned by this function
+#renvoie la meilleure action possible
 
 
 # LEARN EVALUATE 
@@ -54,7 +164,7 @@ def evaluate(s,a):
     return r + (somme s00∈S00) P(s, a, s00)V (s00)
 
 def learnEvaluation(s, a, r, s0, s00):
-    V (s) = V (s) + α(r + V (s00) − V (s))
+    V (s) = V (s) + αlpha(r + V (s00) − V (s))
 
 #The afterstate evaluation function ->  Evaluating afterstates
 # evaluate moves can be regarded as a combination of the action evaluation and the state evaluation 
