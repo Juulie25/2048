61be40b2b36a6912023c2af21555dc9f63d05097
julie pessey <pessey3@orange.fr>
2023-03-16 12:11:51 +0100

    ajout lancement jeu
    non fonctionnel + erreurs

--- pseudoCode.py
+++ pseudoCode.py
@@ -18,232 +18,249 @@ import math
 
 NTUPLES = 17
 
-def __init__(self):
-    # grille complète
-    self.grid = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-    ]
 
-    self.afterstate = self.grid
-    self.addtile = self.grid
+class game2048():
+    def __init__(self):
+        # grille complète
+        self.grid = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
 
-    self.tilesColor = {
-        0: "#BFB3A5",
-        2: "#FAE7E0",
-        4: "#FAE5CA",
-        8: "#FBB17A",
-        16: "#DF9F74",
-        32: "#E08A72",
-        64: "#FD5B42",
-        128: "#FAD177",
-        256: "#F7D067",
-        512: "#F9CA58",
-        1024: "#F9CA58",
-        2048: "#FBC52D",
-        4096: "#F66574",
-        8192: "#F34B5C",
-        16384: "#EB4039",
-        32768: "#70B3D8",
-        65536: "#5EA1E4",
-        131072: "#007FC2"
-    }
-
-    self.actions = {
-        "LEFT",
-        "RIGHT",
-        "UP",
-        "DOWN"
-    }
-
-    self.score = 0
-    self.nbMove = 0
-    self.learn = True
-    self.add_new_tile()
-    self.add_new_tile()
-
-    tuple_l = []
-    tuple_r = []
-    tuple_u = []
-    tuple_d = []
-    for i in range (NTUPLES):
-        tuple_l[i] = {}
-        tuple_r[i] = {}
-        tuple_u[i] = {}
-        tuple_d[i] = {}
-
-    self.v_actions = [tuple_l, tuple_r, tuple_u, tuple_d]
-
-    with open('tuples2048', 'wb') as file:
-        pickle.dump(self.v_actions, file)
-
-    pygame.init()
-    self.font = pygame.font.SysFont("Arial", 36)
-    self.screen = pygame.display.set_mode((400, 500))
-    pygame.display.set_caption("2048 Game")
-
-    while not is_game_over():
-        action = evaluate()
-        self.score += make_move(self.grid,action)
         self.afterstate = self.grid
-        self.nbMove = self.nbMove + 1
-        self.add_new_tile()
         self.addtile = self.grid
-    # if self.learn:
-    #     learn_evaluation(self, action, reward)
 
+        self.tilesColor = {
+            0: "#BFB3A5",
+            2: "#FAE7E0",
+            4: "#FAE5CA",
+            8: "#FBB17A",
+            16: "#DF9F74",
+            32: "#E08A72",
+            64: "#FD5B42",
+            128: "#FAD177",
+            256: "#F7D067",
+            512: "#F9CA58",
+            1024: "#F9CA58",
+            2048: "#FBC52D",
+            4096: "#F66574",
+            8192: "#F34B5C",
+            16384: "#EB4039",
+            32768: "#70B3D8",
+            65536: "#5EA1E4",
+            131072: "#007FC2"
+        }
 
-# Copie proprement les grilles
-def grid_copy(grid):
-    res = [[0 for _ in range(4)] for _ in range(4)]
-    for i in range(0,4):
-        for j in range(0,4):
-            res[i][j] = grid[i][j]
-    return res
+        self.actions = {
+            "LEFT",
+            "RIGHT",
+            "UP",
+            "DOWN"
+        }
 
+        self.score = 0
+        self.nbMove = 0
+        self.learn = True
+        self.add_new_tile()
+        self.add_new_tile()
 
-def is_game_over(self):
-    empty_cells = [(i, j) for i in range(4) for j in range(4) if self.grid[i][j] == 0]
-    if empty_cells: return False
-    for i in range(3):
-        for j in range(3):
-            if self.grid[i][j] == self.grid[i + 1][j] or self.grid[i][j] == self.grid[i][j + 1]:
+        tuple_l = []
+        tuple_r = []
+        tuple_u = []
+        tuple_d = []
+        for i in range (NTUPLES):
+            tuple_l[i] = {}
+            tuple_r[i] = {}
+            tuple_u[i] = {}
+            tuple_d[i] = {}
+
+        self.v_actions = [tuple_l, tuple_r, tuple_u, tuple_d]
+
+        pygame.init()
+        self.font = pygame.font.SysFont("Arial", 36)
+        self.screen = pygame.display.set_mode((400, 500))
+        pygame.display.set_caption("2048 Game")
+
+        with open('tuples2048', 'wb') as file:
+            print("creation fichier")
+            pickle.dump(self.v_actions, file)
+
+        while not self.is_game_over():
+            action = self.evaluate()
+            self.score += self.make_move(self.grid, action)
+            self.afterstate = self.grid
+            self.nbMove = self.nbMove + 1
+            self.add_new_tile()
+            self.addtile = self.grid
+        # if self.learn:
+        #     learn_evaluation(self, action, reward)
+
+    # Copie proprement les grilles
+    def grid_copy(grid):
+        res = [[0 for _ in range(4)] for _ in range(4)]
+        for i in range(0,4):
+            for j in range(0,4):
+                res[i][j] = grid[i][j]
+        return res
+
+    def is_game_over(self):
+        empty_cells = [(i, j) for i in range(4) for j in range(4) if self.grid[i][j] == 0]
+        if empty_cells: return False
+        for i in range(3):
+            for j in range(3):
+                if self.grid[i][j] == self.grid[i + 1][j] or self.grid[i][j] == self.grid[i][j + 1]:
+                    return False
+        # Comparer les dernières lignes et colonnes séparément
+        for i in range(3):
+            if self.grid[3][i] == self.grid[3][i + 1]:
                 return False
-    # Comparer les dernières lignes et colonnes séparément
-    for i in range(3):
-        if self.grid[3][i] == self.grid[3][i + 1]:
-            return False
-        if self.grid[i][3] == self.grid[i + 1][3]:
-            return False
-    return True
+            if self.grid[i][3] == self.grid[i + 1][3]:
+                return False
+        return True
 
+    def evaluate(self, action):
+        reward = 0
+        a = self.v_actions[action]
+        for i in range(17):
+            if self.read_tuple(i) in a:
+                reward += a[i][self.read_tuple(i)]
+        return reward
 
-def evaluate(self, action):
-    reward = 0
-    a = self.v_actions[action]
-    for i in range(17):
-        if read_tuple(i) in a:
-            reward += a[i][read_tuple(i)]
-    return reward
+    # grid, action, reward, grid_afterstate, grid_addtile
+    def learn_evaluation(self, action, reward):
+        i = 1
+        # a partir de S'' on regarde les 4 actions et on prend la meilleure valeur => dans les tables,
+        # calculer les coordonnées pour tous les n-tuples et faire la somme des 3 tuples et on la renvoie
+        # puis récupérer le max entre les 4 valeurs
+        # vnext = maxa0∈A(s00) Va0 (s00)
+        # vnext = "oui"
+        # Va(s) = Va(s) + α(r + vnext − Va(s))
+        # self.v_actions[action] = self.v_actions[action] + alpha*(reward + vnext-self.v_actions[action])
 
+    def move_tiles_left(self, grid):
+        score = 0
+        for row in grid:
+            row.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
+            for i in range(3):
+                if row[i] == row[i + 1]:
+                    row[i] *= 2
+                    score += row[i]
+                    row[i + 1] = 0
+            row.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
+        return score
 
-# grid, action, reward, grid_afterstate, grid_addtile
-def learn_evaluation(self, action, reward):
-    i = 1
-    # a partir de S'' on regarde les 4 actions et on prend la meilleure valeur => dans les tables,
-    # calculer les coordonnées pour tous les n-tuples et faire la somme des 3 tuples et on la renvoie
-    # puis récupérer le max entre les 4 valeurs
-    # vnext = maxa0∈A(s00) Va0 (s00)
-    # vnext = "oui"
-    # Va(s) = Va(s) + α(r + vnext − Va(s))
-    # self.v_actions[action] = self.v_actions[action] + alpha*(reward + vnext-self.v_actions[action])
+    def move_tiles_right(self, grid):
+        score = 0
+        for row in grid:
+            row.sort(key=lambda x: 0 if x == 0 else 1)
+            for i in range(3, 0, -1):
+                if row[i] == row[i - 1]:
+                    row[i] *= 2
+                    score += row[i]
+                    row[i - 1] = 0
+            row.sort(key=lambda x: 0 if x == 0 else 1)
+        return score
 
+    def move_tiles_up(grid):
+        score = 0
+        for j in range(4):
+            col = [grid[i][j] for i in range(4)]
+            col.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
+            for i in range(3):
+                if col[i] == col[i + 1]:
+                    col[i] *= 2
+                    score += col[i]
+                    col[i + 1] = 0
+            col.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
+            for i in range(4):
+                grid[i][j] = col[i]
+        return score
 
-def move_tiles_left(grid):
-    score = 0
-    for row in grid:
-        row.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
-        for i in range(3):
-            if row[i] == row[i + 1]:
-                row[i] *= 2
-                score += row[i]
-                row[i + 1] = 0
-        row.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
-    return score
+    def move_tiles_down(grid):
+        score = 0
+        for j in range(4):
+            col = [grid[i][j] for i in range(4)]
+            col.sort(key=lambda x: 0 if x == 0 else 1)
+            for i in range(3, 0, -1):
+                if col[i] == col[i - 1]:
+                    col[i] *= 2
+                    score += col[i]
+                    col[i - 1] = 0
+            col.sort(key=lambda x: 0 if x == 0 else 1)
+            for i in range(4):
+                grid[i][j] = col[i]
+        return score
 
+    def espace_libere(grid):
+        espace_libre = 0
+        for x in len(grid):
+            for y in len(grid[0]):
+                if grid[x,y] == 0:
+                    espace_libre = espace_libre+1
+        return espace_libre
 
-def move_tiles_right(grid):
-    score = 0
-    for row in grid:
-        row.sort(key=lambda x: 0 if x == 0 else 1)
-        for i in range(3, 0, -1):
-            if row[i] == row[i - 1]:
-                row[i] *= 2
-                score += row[i]
-                row[i - 1] = 0
-        row.sort(key=lambda x: 0 if x == 0 else 1)
-    return score
+    def make_move(grid, action):
+        if action == "right":
+            return move_tiles_right(grid)
+        if action == "left":
+            return self.move_tiles_left(grid)
+        if action == "up":
+            return self.move_tiles_up(grid)
+        if action == "down":
+            return self.move_tiles_down(grid)
 
+    # renvoie la valeur de la ligne
+    def read_tuple(self,i):
+        if i < 4:
+            res0 = math.log2(self.grid[i][0])
+            res1 = math.log2(self.grid[i][1])
+            res2 = math.log2(self.grid[i][2])
+            res3 = math.log2(self.grid[i][3])
+            return str(int(res0)) + str(int(res1)) + str(int(res2)) + str(int(res3))
+        if 4 <= i < 8:
+            res0 = math.log2(self.grid[0][i])
+            res1 = math.log2(self.grid[1][i])
+            res2 = math.log2(self.grid[2][i])
+            res3 = math.log2(self.grid[3][i])
+            return str(int(res0)) + str(int(res1)) + str(int(res2)) + str(int(res3))
+        if i>=8 :
+            if i>7 & i<11:a=0
+            if i>=11 & i<14:a=1
+            if i>=14:a=2
+            res0 = math.log2(self.grid[a][a])
+            res1 = math.log2(self.grid[a][a+1])
+            res2 = math.log2(self.grid[a+1][a])
+            res3 = math.log2(self.grid[a+1][a+1])
+            return str(int(res0)) + str(int(res1)) + str(int(res2)) + str(int(res3))
 
-def move_tiles_up(grid):
-    score = 0
-    for j in range(4):
-        col = [grid[i][j] for i in range(4)]
-        col.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
-        for i in range(3):
-            if col[i] == col[i + 1]:
-                col[i] *= 2
-                score += col[i]
-                col[i + 1] = 0
-        col.sort(key=lambda x: 0 if x == 0 else 1, reverse=True)
+    def draw(self):
+        self.screen.fill((255, 255, 255))
+        self.font = pygame.font.SysFont("Arial", 30)
+        score_text = self.font.render("Score: " + str(self.score), True, (0, 0, 0))
+        move_text = self.font.render("Move: " + str(self.nbMove), True, (0, 0, 0))
+        self.screen.blit(score_text, (10, 10))
+        self.screen.blit(move_text, (10, 45))
+        pygame.draw.rect(self.screen, "#AD9C90", (0, 100, 400, 400))
         for i in range(4):
-            grid[i][j] = col[i]
-    return score
+            for j in range(4):
+                pygame.draw.rect(self.screen, self.tilesColor[self.grid[i][j]], (j * 100 + 10, i * 100 + 110, 80, 80))
+                if self.grid[i][j] != 0:
+                    size = 0 if len(str(self.grid[i][j])) - 4 < 0 else len(str(self.grid[i][j])) - 3
+                    self.font = pygame.font.SysFont("Arial", 32 - 4 * size)
+                    text = self.font.render(str(self.grid[i][j]), True, (0, 0, 0))
+                    text_rect = text.get_rect()
+                    text_rect.center = (j * 100 + 50, i * 100 + 150)
+                    self.screen.blit(text, text_rect)
+        pygame.display.update()
+
+    def run(self):
+        self.draw()
 
 
-def move_tiles_down(grid):
-    score = 0
-    for j in range(4):
-        col = [grid[i][j] for i in range(4)]
-        col.sort(key=lambda x: 0 if x == 0 else 1)
-        for i in range(3, 0, -1):
-            if col[i] == col[i - 1]:
-                col[i] *= 2
-                score += col[i]
-                col[i - 1] = 0
-        col.sort(key=lambda x: 0 if x == 0 else 1)
-        for i in range(4):
-            grid[i][j] = col[i]
-    return score
-
-
-def espace_libere(grid):
-    espace_libre = 0
-    for x in len(grid):
-        for y in len(grid[0]):
-            if grid[x,y] == 0:
-                espace_libre = espace_libre+1
-    return espace_libre
-
-
-def make_move(grid, action):
-    if action == "right":
-        return move_tiles_right(grid)
-    if action == "left":
-        return move_tiles_left(grid)
-    if action == "up":
-        return move_tiles_up(grid)
-    if action == "down":
-        return move_tiles_down(grid)
-
-
-# renvoie la valeur de la ligne
-def read_tuple(self,i):
-    if i < 4:
-        res0 = math.log2(self.grid[i][0])
-        res1 = math.log2(self.grid[i][1])
-        res2 = math.log2(self.grid[i][2])
-        res3 = math.log2(self.grid[i][3])
-        return str(int(res0)) + str(int(res1)) + str(int(res2)) + str(int(res3))
-    if 4 <= i < 8:
-        res0 = math.log2(self.grid[0][i])
-        res1 = math.log2(self.grid[1][i])
-        res2 = math.log2(self.grid[2][i])
-        res3 = math.log2(self.grid[3][i])
-        return str(int(res0)) + str(int(res1)) + str(int(res2)) + str(int(res3))
-    if i>=8 :
-        if i>7 & i<11:a=0
-        if i>=11 & i<14:a=1
-        if i>=14:a=2
-        res0 = math.log2(self.grid[a][a])
-        res1 = math.log2(self.grid[a][a+1])
-        res2 = math.log2(self.grid[a+1][a])
-        res3 = math.log2(self.grid[a+1][a+1])
-        return str(int(res0)) + str(int(res1)) + str(int(res2)) + str(int(res3))
-
-
+game = game2048()
+game.run()
 #==============================PSEUDO CODE ==============================#
 # def playGame() :
 #     score = 0
